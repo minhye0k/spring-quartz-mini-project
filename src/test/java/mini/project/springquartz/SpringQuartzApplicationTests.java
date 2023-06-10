f184f71eedaac56b3d807a00d0f193ad0b26b093
minhyeok <omh34658981@gmail.com>
2023-06-10 16:42:51 +0900

    initial commit

---
+++ src/test/java/mini/project/springquartz/SpringQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.project.springquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
